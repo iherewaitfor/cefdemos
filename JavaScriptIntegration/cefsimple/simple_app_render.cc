c915b92938150f2f9b9cb6b0da4b1b35e0c8f0b3
yangkemin <iherewaitfor@163.com>
2023-06-09 21:57:11 +0800

    update JavaScriptIntegration. JS Objects width CefV8Accessor.

--- JavaScriptIntegration/cefsimple/simple_app_render.cc
+++ JavaScriptIntegration/cefsimple/simple_app_render.cc
@@ -31,6 +31,52 @@ public:
     }
     IMPLEMENT_REFCOUNTING(MyCefV8Handler);
 };
+
+class MyV8Accessor : public CefV8Accessor {
+public:
+    MyV8Accessor() { }
+
+    virtual bool Get(const CefString& name,
+        const CefRefPtr<CefV8Value> object,
+        CefRefPtr<CefV8Value>& retval,
+        CefString& exception) override {
+        if (name == "myval") {
+            // Return the value.
+            retval = CefV8Value::CreateString(myval_);
+            return true;
+        }
+
+        // Value does not exist.
+        return false;
+    }
+
+    virtual bool Set(const CefString& name,
+        const CefRefPtr<CefV8Value> object,
+        const CefRefPtr<CefV8Value> value,
+        CefString& exception) override {
+        if (name == "myval") {
+            if (value->IsString()) {
+                // Store the value.
+                myval_ = value->GetStringValue();
+            }
+            else {
+                // Throw an exception.
+                exception = "Invalid value type";
+            }
+            return true;
+        }
+
+        // Value does not exist.
+        return false;
+    }
+
+    // Variable used for storing the value.
+    CefString myval_;
+
+    // Provide the reference counting implementation for this class.
+    IMPLEMENT_REFCOUNTING(MyV8Accessor);
+};
+
 void SimpleAppRender::OnWebKitInitialized() {
     CefRefPtr<CefV8Handler> handler = new MyCefV8Handler();
     ::MessageBox(0, 0, 0, 0);
@@ -51,11 +97,23 @@ void SimpleAppRender::OnContextCreated(
     CefRefPtr<CefFrame> frame,
     CefRefPtr<CefV8Context> context) {
     // Retrieve the context's window object.
-    CefRefPtr<CefV8Value> object = context->GetGlobal();
+    CefRefPtr<CefV8Value> window = context->GetGlobal();
 
     // Create a new V8 string value. See the "Basic JS Types" section below.
     CefRefPtr<CefV8Value> str = CefV8Value::CreateString("My Value in window!");
 
     // Add the string to the window object as "window.myval". See the "JS Objects" section below.
-    object->SetValue("myval", str, V8_PROPERTY_ATTRIBUTE_NONE);
+    window->SetValue("myval", str, V8_PROPERTY_ATTRIBUTE_NONE);
+
+
+    CefRefPtr<CefV8Accessor> accessor = new MyV8Accessor();
+    CefRefPtr<CefV8Value> obj = CefV8Value::CreateObject(accessor, nullptr);
+    
+    //In order for a value to be passed to the accessor 
+    //it must be set using the SetValue() method variant that accepts AccessControl and PropertyAttribute arguments.
+    obj->SetValue("myval", V8_ACCESS_CONTROL_DEFAULT,
+        V8_PROPERTY_ATTRIBUTE_NONE);
+    //¸øobjectÌí¼ÓÊôÐÔ
+    obj->SetValue("myval", CefV8Value::CreateString("My Value in myObject!"), V8_PROPERTY_ATTRIBUTE_NONE);
+    window->SetValue("myObjcet", obj, V8_PROPERTY_ATTRIBUTE_NONE);
 }
