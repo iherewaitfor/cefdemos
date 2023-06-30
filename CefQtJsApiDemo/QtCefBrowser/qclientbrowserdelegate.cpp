1bf58643cb47fae1fc787dd91372df722eff2499
yangkemin <iherewaitfor@163.com>
2023-06-30 14:40:56 +0800

    update CefQtJsApiDemo. browser进程添加命令行参数 --renderer-process-limit=1，只使用一个render进程，方便进行render进程调试。

--- CefQtJsApiDemo/QtCefBrowser/qclientbrowserdelegate.cpp
+++ CefQtJsApiDemo/QtCefBrowser/qclientbrowserdelegate.cpp
@@ -7,4 +7,7 @@ void QClientBrowserDelegate::OnContextInitialized(CefRefPtr<QCefBrowserApp> app)
 void QClientBrowserDelegate::OnBeforeCommandLineProcessing(
     CefRefPtr<QCefBrowserApp> app,
     CefRefPtr<CefCommandLine> command_line) {
+    if (!command_line->HasSwitch("renderer-process-limit")) {
+        command_line->AppendSwitchWithValue("renderer-process-limit", "1");
+    }
 }
